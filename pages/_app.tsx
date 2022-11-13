677490fc11877d76394da12282bae2e0c3020046
Max Proske <max@mproske.com>
2022-11-13 10:42:18 -0800

    Load variable Google font using `@next/font`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Oswald } from '@next/font/google'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+const oswald = Oswald()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className={oswald.className}>
+    <Component {...pageProps} />
+  </div>
 }
