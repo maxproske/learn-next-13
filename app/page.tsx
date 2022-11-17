7eb9b6ea00c807bb7c7afc9fd97476a5221b772e
Max Proske <max@mproske.com>
2022-11-16 19:17:43 -0800

    SSR!

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,29 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+interface Time {
+  unixtime: number
+}
 
-export default function HomePage() {
+async function getCurrentTime() {
+  // This request should be refetched on every request.
+  // Similar to `getServerSideProps`
+  const res = await fetch('https://worldtimeapi.org/api/timezone/America/Vancouver', {
+    cache: 'no-store',
+  })
+
+  const time: Promise<Time> = await res.json()
+
+  return time
+}
+
+// Everything here happens on the server!!
+export default async function HomePage() {
+  // No Next.js API
+  // No serialization
+  const time = await getCurrentTime()
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div>
+      <h1>Time in Vancouver (SSR)</h1>
+      <p>{new Date(time.unixtime * 1000).toString()}</p>
     </div>
   )
 }
