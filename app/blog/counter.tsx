eda800a5c1a8e2956d3b1c2dfea5de5d716be0fe
Max Proske <max@mproske.com>
2022-11-16 19:37:55 -0800

    Get cookies and headers from `next/headers`

--- app/blog/counter.tsx
+++ app/blog/counter.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 export default function Counter() {
   const [count, setCount] = useState(0)
 
+  console.log('hi from the server & client')
+
   return (
     <div>
       <button onClick={() => setCount((prevState) => prevState + 1)}>{count}</button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
+import { cookies, headers } from 'next/headers'
+
 interface Time {
   unixtime: number
 }
 
 async function getCurrentTime() {
+  const nextCookies = cookies()
+  const language = nextCookies.get('language')?.value
+
+  const nextHeaders = headers()
+  const referer = nextHeaders.get('referer')
+
+  console.log({ language, referer })
+
   // This request should be refetched on every request.
   // Similar to `getServerSideProps`
   const res = await fetch('https://worldtimeapi.org/api/timezone/America/Vancouver', {
     cache: 'no-store',
+    headers: {
+      'x-language': `${language}`,
+    },
   })
 
   const time: Promise<Time> = await res.json()
@@ -20,10 +33,11 @@ export default async function HomePage() {
   // No serialization
   const time = await getCurrentTime()
 
+  console.log('hi from the server')
+
   return (
     <div>
-      <h1>Time in Vancouver (SSR)</h1>
-      <p>{new Date(time.unixtime * 1000).toString()}</p>
+      <p>Date: {new Date(time.unixtime * 1000).toString()}</p>
     </div>
   )
 }
